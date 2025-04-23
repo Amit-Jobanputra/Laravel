ebc3d07cd2afcb62028058c1f0057c93fbed1b3a
Amit-Jobanputra <amit.jobanputra75@gmail.com>
2025-04-23 07:58:29 +0530

    Portfolio Upddate

--- Portfolio/routes/web.php
+++ Portfolio/routes/web.php
@@ -20,3 +20,24 @@ Route::get('/Work', [Portfolio::class ,'work']  );
 Route::get('/Contact',[Portfolio::class ,'contact']  );
 Route::get('/About',  [Portfolio::class ,'about']  );
 Route::get('/Skills', [Portfolio::class ,'skill']  );
+Route::get('/billing',function(){
+    return view('billing');
+});
+Route::get('/dashboard',function(){
+    return view('dashboard');
+});
+Route::get('/login',function(){
+    return view('sign-in');
+});
+Route::get('/sign',function(){
+    return view('sign-up');
+});
+Route::get('/profile',function(){
+    return view('profile');
+});
+Route::get('/notifications',function(){
+    return view('notifications');
+});
+Route::get('/tables',function(){
+    return view('tables');
+});
